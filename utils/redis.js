f6298ebb1fd397cf5597c4833c2dbc21a94ee5e6
Nomad-Rob <rfarley89@gmail.com>
2024-03-13 13:05:53 -0500

    reworked redis.js and imported prmisify.
    Next issue is with getting stats

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,37 @@
 // Task 0 - Creating Redis Client
-const redis = require('redis');
-const { promisify } = require('util');
+import redis from 'redis';
+import { promisify } from 'util';
 
+// RedisClient class
 class RedisClient {
-  // Create a new instance of the Redis client
   constructor() {
     this.client = redis.createClient();
-
-    // Log when the client is connected to the server
-    this.client.on('error', (err) => {
-      console.log(`Redis client not connected to server dude ${err}`);
-    });
-
-    // Convert callback-based Redis methods to promise-based methods
-    this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
+    this.client.on('error', (error) => console.error(`Redis client not connected: ${error}`));
   }
 
-  // Check if the client is connected to the server
+  // Check if the connection is alive
   isAlive() {
     return this.client.connected;
   }
 
-  // Get the value of a key from the Redis instance
+  // Get value from key
   async get(key) {
-    return this.getAsync(key);
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
   }
 
-  // Set the value of a key in the Redis instance
+  // Set key with value and duration
   async set(key, value, duration) {
-    return this.setAsync(key, value, 'EX', duration);
+    const asyncSet = promisify(this.client.setex).bind(this.client);
+    return asyncSet(key, duration, value);
   }
 
-  // Delete a key from the Redis instance
+  // Delete key
   async del(key) {
-    return this.delAsync(key);
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    return asyncDel(key);
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
