4e3bae10e6bfec1ffcc90871a59086985c3cdaf1
Nomad-Rob <rfarley89@gmail.com>
2024-03-13 15:01:36 -0500

    eslint fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Setting up the Express Server
 
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
