3033435336ba5fdf57c4c9223adc5c7e551beb05
Shadi-Shwiyat <raz611103@gmail.com>
2024-03-15 21:16:11 -0500

    fixed postupload delete

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,14 +1,83 @@
-// import { v4 as uuidv4 } from 'uuid';
-// import fs from 'fs';
-// import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import path from 'path';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class FilesController {
-  // static async postUpload(req, res) {
+  static async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      console.log('No userId or invalid');
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-  // }
+    const {
+      name, type, parentId = '0', isPublic = false, data,
+    } = req.body;
+    if (!name) {
+      console.log('Missing name');
+      return res.status(400).json({ error: 'Missing name' });
+    }
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      console.log('Missing type');
+      return res.status(400).json({ error: 'Missing type' });
+    }
+    if (!data && type !== 'folder') {
+      console.log('Missing data');
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    let parentFile = null;
+    if (parentId !== '0') {
+      parentFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(parentId) });
+      if (!parentFile) {
+        console.log('Parent not found');
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        console.log('Parent is not a folder');
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const fileData = {
+      userId: new ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId: parentId !== '0' ? new ObjectId(parentId) : 0,
+    };
+
+    if (type !== 'folder') {
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+      const fileName = uuidv4();
+      const filePath = path.join(folderPath, fileName);
+
+      const fileBuffer = Buffer.from(data, 'base64');
+      fs.writeFileSync(filePath, fileBuffer);
+
+      fileData.localPath = filePath;
+    }
+
+    const newFile = await dbClient.db.collection('files').insertOne(fileData);
+
+    console.log('newFile is:', newFile);
+    return res.status(201).json({
+      id: newFile.insertedId,
+      userId: fileData.userId,
+      name: fileData.name,
+      type: fileData.type,
+      isPublic: fileData.isPublic,
+      parentId: fileData.parentId,
+      ...(type !== 'folder' && { localPath: fileData.localPath }),
+    });
+  }
 
   static async getShow(req, res) {
     const token = req.headers['x-token'];
