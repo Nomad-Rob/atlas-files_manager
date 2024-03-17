84f4bf5ab121000d02ea19c712d6ac5fefa0d980
Shadi-Shwiyat <raz611103@gmail.com>
2024-03-17 02:27:40 -0500

    fixing linter errors

--- utils/redis.js
+++ utils/redis.js
@@ -6,28 +6,30 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.connected = true;
-    this.client.on('error', (error) => {console.error(`Redis client not connected: ${error}`)});
+    this.client.on('error', (error) => { console.error(`Redis client not connected: ${error}`); });
     this.client.on('connect', () => {
       resolve();
-      // return this.isAlive();
-    })
+    });
   }
+
   // Check if the connection is alive
   isAlive() {
     // console.log("The client is connected, true or false?:", this.client.connected);
-    return this.connected;
+    return this.client.connected;
   }
+
   // Get value from key
   async get(key) {
     const asyncGet = promisify(this.client.get).bind(this.client);
     return asyncGet(key);
   }
+
   // Set key with value and duration
   async set(key, value, duration) {
     const asyncSet = promisify(this.client.setex).bind(this.client);
     return asyncSet(key, duration, value);
   }
+
   // Delete key
   async del(key) {
     const asyncDel = promisify(this.client.del).bind(this.client);
